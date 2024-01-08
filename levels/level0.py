34f2e8a6a0327812587b1b3a9e91359d03d196ba
Владислав <151929494+FIADELE@users.noreply.github.com>
2024-01-08 17:52:25 +0500

    Add files via upload
    Adding level sistem

---
+++ levels/level0.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame import Surface, Vector2
+from constants import *
+
+
+def level_0_loop(screen: Surface) -> LevelId:
+    def start() -> None:
+        print('Start pressed')
+
+    clock = pygame.time.Clock()
+
+    win_size = screen.get_size()
+
+    while True:
+        clock.tick(MAX_FPS)
+        screen.fill(BLACK)
+        mouse_pos = pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+            match event.type:
+                case pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                case pygame.KEYDOWN:
+                    match event.key:
+                        case pygame.K_ESCAPE:
+                            return LEVEL_0
+
+        pygame.display.flip()
